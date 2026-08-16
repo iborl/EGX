14d30127ae3d9f09c622256512a076fd398633da
borl <314047290+iborl@users.noreply.github.com>
2026-08-16 00:37:23 -0400

    refactor(config): add comment to config/arch.h clarifying that code paths are unaffected note: this is because there is no architecture-specific C code that exists yet
    refactor(config): remove config/boot.h because it serves no purpose at the moment
    note: bootloader choice is planned to be configurable via Makefile
    
    refactor: add comment to type.h clarifying that only AMD64 type definitions exist

--- src/config/arch.h
+++ src/config/arch.h
@@ -7,4 +7,6 @@
 // Defining CONFIG_ARCH_IA32 would enable IA32 code paths and typing
 // #define CONFIG_ARCH_IA32
 
+// So far, enabling an architecture macro would not disable or enable code paths yet, it only decides what type definitions are used
+
 #endif
--- src/type.h
+++ src/type.h
@@ -1,16 +1,17 @@
 // type.h
 // used as a centralized header file for providing type definitions based on the given architecture
-//
+
 #ifndef EGX_TYPE_H
 #define EGX_TYPE_H
 
 #include "config/arch.h"
-
-// 
+ 
 #if defined(CONFIG_ARCH_AMD64)
 #include "arch/x86_64/types.h"
 #else
 #error "[EGX]: no architecture selected for type definitions"
 #endif
+
+// as of right now, AMD64 is the only architecture that has type definitions
 
 #endif
